1b469d78ad81824f3f652a43f0b2892ca051fa7c
bulbajs <93475048+bulbajs@users.noreply.github.com>
2024-11-25 23:12:50 +0300

    Add files via upload
    +custom filters

--- d3_models_pw/simpleapp/models.py
+++ d3_models_pw/simpleapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator, MinValueValidator
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
